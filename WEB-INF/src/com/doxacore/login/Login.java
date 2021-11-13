803302413abaffc93986e4f658b00f7299cd7de2
RickSpider <rrgi89@hotmail.com>
2021-11-13 09:21:12 -0300

    Creacion de Clase Login

---
+++ WEB-INF/src/com/doxacore/login/Login.java
@@ -0,0 +1,23 @@
+package com.doxacore.login;
+
+public class Login {
+	
+	private String user;
+	private String passoword;
+	
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassoword() {
+		return passoword;
+	}
+	public void setPassoword(String passoword) {
+		this.passoword = passoword;
+	}
+	
+	
+
+}
