493b02ba7ed768f21cefa06abd1326bee0758fd6
RickSpider <rrgi89@hotmail.com>
2022-03-19 17:32:29 -0300

    cambio por visualizacion automatica

--- WEB-INF/src/com/doxacore/main/menu/NavigationTitle.java
+++ WEB-INF/src/com/doxacore/main/menu/NavigationTitle.java
@@ -3,13 +3,11 @@ package com.doxacore.main.menu;
 public class NavigationTitle {
 
 	private String title;
-	private boolean visible;
 	private String icon;
 	
 	public NavigationTitle(String title, boolean visible, String icon) {
 		super();
 		this.title = title;
-		this.visible = visible;
 		this.icon = icon;
 	}
 	
@@ -17,27 +15,17 @@ public class NavigationTitle {
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public boolean isVisible() {
-		return visible;
-	}
-	public void setVisible(boolean visible) {
-		this.visible = visible;
-	}
-
 
 	public String getIcon() {
 		return icon;
 	}
 
-
 	public void setIcon(String icon) {
 		this.icon = icon;
 	}
-	
-	
-	
 	
 }
